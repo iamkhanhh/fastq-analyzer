98fde4fec6a1eff1d6963abaf6c3b67081056316
iamkhanhh <khanh9102004@gmail.com>
2025-09-09 16:01:32 +0700

    set up models and constants

---
+++ src/constants/fastq.constant.ts
@@ -0,0 +1,20 @@
+export const VCF_OUTPUT_FILE = 'analysis.fastq.vcf'
+
+export const SAM_FILE = 'analysis.sam'
+
+export const BAM_FILE = 'analysis.bam'
+
+export const SORTED_BAM_FILE = 'analysis.sorted.bam'
+
+export const FASTQ1_FILE = 'fastq_1.fastq'
+
+export const FASTQ1_FILE_ZIP = 'fastq_1.fastq.gz'
+
+export const FASTQ2_FILE = 'fastq_2.fastq'
+
+export const FASTQ2_FILE_ZIP = 'fastq_2.fastq.gz'
+
+// Commands
+export const SAMTOOLS_CMD = 'samtools'
+export const BWA_CMD = 'bwa'
+export const BCFTOOLS_CMD = 'bcftools'
